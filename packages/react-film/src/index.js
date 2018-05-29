fe97adb93b8cf5976502106482fed671398ce9d7
William Wong <compulim@hotmail.com>
2018-05-29 00:56:24 -0700

    Update styles

--- packages/react-film/src/index.js
+++ packages/react-film/src/index.js
@@ -1,7 +1,11 @@
+import { css } from 'glamor';
 import React from 'react';
 
+const ROOT_CSS = css({
+});
+
 export default props =>
-  <div>
+  <div className={ ROOT_CSS }>
     <ul>
       <li><img src="/image/01.jpg" /></li>
       <li><img src="/image/02.jpg" /></li>
