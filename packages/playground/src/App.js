13cfacf8a97f47eb73ccffa21bb7245b017e2a5e
William Wong <compulim@hotmail.com>
2018-05-29 00:59:02 -0700

    Add more photos

--- packages/playground/src/App.js
+++ packages/playground/src/App.js
@@ -5,7 +5,19 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Film />
+        <Film>
+          <img src="image/01.jpg" />
+          <img src="image/02.jpg" />
+          <img src="image/03.jpg" />
+          <img src="image/04.jpg" />
+          <img src="image/05.jpg" />
+          <img src="image/06.jpg" />
+          <img src="image/07.jpg" />
+          <img src="image/08.jpg" />
+          <img src="image/09.jpg" />
+          <img src="image/10.jpg" />
+          <img src="image/11.jpg" />
+        </Film>
       </div>
     );
   }
--- packages/react-film/src/index.js
+++ packages/react-film/src/index.js
@@ -12,8 +12,10 @@ const ROOT_CSS = css({
 export default props =>
   <div className={ ROOT_CSS }>
     <ul>
-      <li><img src="/image/01.jpg" /></li>
-      <li><img src="/image/02.jpg" /></li>
-      <li><img src="/image/03.jpg" /></li>
+      {
+        React.Children.map(props.children, child =>
+          <li>{ child }</li>
+        )
+      }
     </ul>
   </div>
