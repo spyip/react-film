22dbc1631065ab0937da516c9e10ba7aeef82389
William Wong <compulim@hotmail.com>
2018-06-02 14:48:27 -0700

    Fix build break

--- packages/playground/src/App.js
+++ packages/playground/src/App.js
@@ -12,17 +12,17 @@ class App extends Component {
     return (
       <div>
         <Film className={ FILM_CSS + '' }>
-          <img src="image/01.jpg" />
-          <img src="image/02.jpg" />
-          <img src="image/03.jpg" />
-          <img src="image/04.jpg" />
-          <img src="image/05.jpg" />
-          <img src="image/06.jpg" />
-          <img src="image/07.jpg" />
-          <img src="image/08.jpg" />
-          <img src="image/09.jpg" />
-          <img src="image/10.jpg" />
-          <img src="image/11.jpg" />
+          <img alt="Cat 01" src="image/01.jpg" />
+          <img alt="Cat 02" src="image/02.jpg" />
+          <img alt="Cat 03" src="image/03.jpg" />
+          <img alt="Cat 04" src="image/04.jpg" />
+          <img alt="Cat 05" src="image/05.jpg" />
+          <img alt="Cat 06" src="image/06.jpg" />
+          <img alt="Cat 07" src="image/07.jpg" />
+          <img alt="Cat 08" src="image/08.jpg" />
+          <img alt="Cat 09" src="image/09.jpg" />
+          <img alt="Cat 10" src="image/10.jpg" />
+          <img alt="Cat 11" src="image/11.jpg" />
         </Film>
       </div>
     );
