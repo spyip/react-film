cf724f657b9b65a047f4cb91a83c18e6a430b547
William Wong <compulim@hotmail.com>
2018-06-03 01:59:20 -0700

    Flipper button

--- packages/react-film/src/film.js
+++ packages/react-film/src/film.js
@@ -33,6 +33,7 @@ const ROOT_CSS = css({
     overflowY: 'hidden',
     MsOverflowStyle: 'none',
     touchAction: 'pan-x',
+    WebkitOverflowScrolling: 'touch',
 
     '&::-webkit-scrollbar': {
       display: 'none'
@@ -52,6 +53,7 @@ const ROOT_CSS = css({
     border: 0,
     color: 'White',
     height: '100%',
+    maxWidth: '10%',
     opacity: 0,
     position: 'absolute',
     top: 0,
@@ -60,26 +62,35 @@ const ROOT_CSS = css({
 
     '&.left': { left: 0 },
     '&.right': { right: 0 }
-  },
-
-  '& > .debug': {
-    backgroundColor: 'Red',
-    color: 'White',
-    left: 0,
-    position: 'absolute',
-    top: 0
   }
 });
+
+function best(array, scorer) {
+  return array.reduce((best, item, index) => {
+    const score = scorer.call(array, item, index);
+
+    if (score > best.score) {
+      return { index, score };
+    } else {
+      return best;
+    }
+  }, { index: -1, score: -Infinity }).index;
+}
 
 export default class Film extends React.Component {
   constructor(props, context) {
     super(props, context);
 
+    this.handleNextClick = this.handleNextClick.bind(this);
+    this.handlePrevClick = this.handlePrevClick.bind(this);
     this.handleScroll = this.handleScroll.bind(this);
     this.saveStrip = this.saveStrip.bind(this);
 
     this.state = {
-      stripRef: null
+      index: -1,
+      stripRef: null,
+      viewLeft: 0,
+      viewWidth: 0
     };
   }
 
@@ -87,8 +98,56 @@ export default class Film extends React.Component {
     clearTimeout(this.hideScrollBarTimeout);
   }
 
-  handleScroll() {
-    this.setState(() => ({ scrolling: true }));
+  getView() {
+    const target = ReactDOM.findDOMNode(this.state.stripRef);
+
+    if (target) {
+      const { scrollLeft } = target;
+      const items = target.querySelectorAll('ul > li');
+      const index = [].findIndex.call(items, item => item.offsetLeft + item.offsetWidth > scrollLeft);
+
+      if (~index) {
+        const item = items[index];
+
+        return {
+          indexFraction: index + (scrollLeft - item.offsetLeft) / item.offsetWidth,
+          items,
+          target,
+        };
+      }
+    }
+  }
+
+  handleNextClick() {
+    const { indexFraction, items, target } = this.getView();
+
+    if (~indexFraction) {
+      const nextIndex = Math.floor(indexFraction) + 1;
+
+      if (nextIndex < items.length) {
+        target.scrollLeft = items[nextIndex].offsetLeft;
+      }
+    }
+  }
+
+  handlePrevClick() {
+    const { indexFraction, items, target } = this.getView();
+
+    if (~indexFraction) {
+      const nextIndex = Math.ceil(indexFraction) - 1;
+
+      if (nextIndex >= 0) {
+        target.scrollLeft = items[nextIndex].offsetLeft;
+      } else {
+        target.scrollLeft = 0;
+      }
+    }
+  }
+
+  handleScroll({ target }) {
+    this.setState(() => ({
+      scrolling: true
+    }));
 
     this.hideScrollBarTimeout = setTimeout(() => {
       this.hideScrollBarTimeout = null;
@@ -115,10 +174,16 @@ export default class Film extends React.Component {
             }
           </ul>
         </div>
-        <button className="flipper left">
+        <button
+          className="flipper left"
+          onClick={ this.handlePrevClick }
+        >
           { props.leftFlipper || '<' }
         </button>
-        <button className="flipper right">
+        <button
+          className="flipper right"
+          onClick={ this.handleNextClick }
+        >
           { props.rightFlipper || '>' }
         </button>
         <ScrollBar
