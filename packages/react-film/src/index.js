c26eedfac3358be5aec7f2ef8cfe1fa4120bb1bb
William Wong <compulim@hotmail.com>
2018-06-07 15:41:58 -0700

    Export custom components

--- packages/react-film/src/index.js
+++ packages/react-film/src/index.js
@@ -1,14 +1,20 @@
 import BasicFilm from './BasicFilm';
+import Dots from './Dots';
 import FilmContainer from './FilmContainer';
 import FilmContext from './FilmContext';
 import FilmStrip from './FilmStrip';
+import Flipper from './Flipper';
 import memoize from './memoize';
+import ScrollBar from './ScrollBar';
 
 export default BasicFilm
 
 export {
+  Dots,
   FilmContainer,
   FilmContext,
   FilmStrip,
-  memoize
+  Flipper,
+  memoize,
+  ScrollBar
 }
