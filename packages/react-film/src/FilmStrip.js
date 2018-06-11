5d8b833b3c851f77f805a394f556fb3d72e33cfd
William Wong <compulim@hotmail.com>
2018-06-11 01:43:41 -0700

    Cleanup DOM

--- packages/react-film/src/FilmStrip.js
+++ packages/react-film/src/FilmStrip.js
@@ -5,35 +5,30 @@ import React from 'react';
 import FilmContext from './FilmContext';
 
 const ROOT_CSS = css({
-  overflow: 'hidden',
-  position: 'relative',
+  height: '100%',
+  MsOverflowStyle: 'none',
+  overflowX: 'scroll',
+  overflowY: 'hidden',
+  touchAction: 'pan-x',
+  WebkitOverflowScrolling: 'touch',
 
-  '& > .strip': {
-    height: '100%',
-    MsOverflowStyle: 'none',
-    overflowX: 'scroll',
-    overflowY: 'hidden',
-    touchAction: 'pan-x',
-    WebkitOverflowScrolling: 'touch',
+  '&::-webkit-scrollbar': {
+    display: 'none'
+  },
 
-    '&::-webkit-scrollbar': {
-      display: 'none'
-    },
+  '& > ul': {
+    display: 'flex',
+    listStyleType: 'none',
+    margin: 0,
+    padding: 0,
 
-    '& > ul': {
-      display: 'flex',
-      listStyleType: 'none',
-      margin: 0,
-      padding: 0,
-
-      '& > li': {
-        display: 'flex'
-      }
+    '& > li': {
+      display: 'flex'
     }
   }
 });
 
-class Call extends React.Component {
+class CallFunction extends React.Component {
   componentDidMount() {
     const { props: { arg, fn } } = this;
 
@@ -54,16 +49,16 @@ class Call extends React.Component {
 export default props =>
   <FilmContext.Consumer>
     { context =>
-      <div className={ classNames(ROOT_CSS + '', props.className) }>
-        <div className="strip" ref={ context.setFilmStripRef }>
+      <React.Fragment>
+        <div className={ classNames(ROOT_CSS + '', props.className) } ref={ context.setFilmStripRef }>
           <ul>
             { React.Children.map(props.children, child => <li>{ child }</li>) }
           </ul>
         </div>
-        <Call
+        <CallFunction
           arg={ React.Children.count(props.children) }
           fn={ context.setNumItems }
         />
-      </div>
+      </React.Fragment>
     }
   </FilmContext.Consumer>
