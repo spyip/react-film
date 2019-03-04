14cfd823cd9cf212c9072c78417b102b3ef14b84
William Wong <compulim@hotmail.com>
2019-03-03 23:18:14 -0800

    Retrofit non-React element

---
+++ packages/bundle/webpack.config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path');
+const Visualizer = require('webpack-visualizer-plugin');
+
+module.exports = {
+  entry: {
+    'react-film.min': './lib/index.js'
+  },
+  mode: 'production',
+  output: {
+    filename: '[name].js',
+    library: 'ReactFilm',
+    libraryTarget: 'umd',
+    path: resolve(__dirname, 'dist')
+  },
+  plugins: [new Visualizer()]
+};
