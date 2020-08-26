d3e9a2ec2650a5347f0c9434c423075b3d124403
William Wong <compulim@hotmail.com>
2020-08-26 08:20:18 -0700

    Clean up

--- packages/component/src/hooks/internal/useTimeout.js
+++ packages/component/src/hooks/internal/useTimeout.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 export default function useTimeout(onTrigger, duration) {
   useEffect(() => {
     if (onTrigger) {
-      const timeout = setTimeout(() => onTrigger(), duration);
+      const timeout = setTimeout(onTrigger, duration);
 
       return () => clearTimeout(timeout);
     }
