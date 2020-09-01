67027a9f98effbc837d029edda01ab3bc3516c5d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-01 09:48:23 +0000

    Deploy spyip/react-film to github.com/spyip/react-film.git:gh-pages

---
+++ precache-manifest.db32cfc445226391062b5ba065b288f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc558c645f701794425edd45da3f137d",
+    "url": "/react-film/index.html"
+  },
+  {
+    "revision": "4f2fd567877b48e34328",
+    "url": "/react-film/static/js/2.40f12973.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/react-film/static/js/2.40f12973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eb1263d986138257b16",
+    "url": "/react-film/static/js/main.7896d4e3.chunk.js"
+  },
+  {
+    "revision": "ce869e0daeba8f06a5a9",
+    "url": "/react-film/static/js/runtime-main.aa954de8.js"
+  }
+]);
